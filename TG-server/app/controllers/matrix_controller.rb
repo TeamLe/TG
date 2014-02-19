2cfc36377bf68c5079b491293854291e6860f3d9
Leonardo <leonardowistuba@gmail.com>
2014-02-18 23:48:38 -0300

    Termina com o primeiro exercicio proposto de rails

--- TG-server/app/controllers/matrix_controller.rb
+++ TG-server/app/controllers/matrix_controller.rb
@@ -1,11 +1,27 @@
 class MatrixController < ApplicationController
 
 	def change
-	banana = "oi"
+	
+	color = "red"
+
+	if !params[:color].nil? && !params[:color].empty?
+		color = params[:color]
+		puts color
+		if color.eql?("#ff0000")
+			color = "#00ff00"
+		elsif color.eql?("#00ff00")
+			color = "#0000ff"
+		elsif color.eql?("#0000ff")
+			color = "#000000"			
+		elsif color.eql?("#000000")
+			color = "#ff0000"			
+		end
+
+	end 
 
 	respond_to do |format|
 	  format.html # change.html.erb
-	  format.json { render :json => {"banana" => "coco"} }
+	  format.json { render :json => {color: color, matrix_id: params[:matrix_id]} }
 	end
 		
 	end
--- TG-server/public/js/application.js
+++ TG-server/public/js/application.js
@@ -2,16 +2,39 @@ $(document).ready(function () {
 });
 
 $('.matrix-item').click(function() {
-	sendColor();
+	var rgbColor = $(this).css('backgroundColor');
+	var hexColor = rgb2hex(rgbColor);
+
+	var elementId = $(this).attr('id');
+
+	sendColor(hexColor, elementId);
 });
 
-function sendColor() {
+function sendColor(color, elementId) {
 	$.ajax({
-			url: 'http://localhost:3000/matrix/change.json',
-			type: 'GET',
-			data: {color:"red"},
+		url: 'http://localhost:3000/matrix/change.json',
+		type: 'GET',
+		data: {color:color,
+			matrix_id: elementId},
 			success: function (data) {
-				alert(data.banana);
+				changeColor(data.color, data.matrix_id);
 			}
 		});
 };
+
+function changeColor(color, elementId) {
+	$('#' + elementId).css('background-color', color);
+};
+
+var hexDigits = new Array
+("0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"); 
+
+//Function to convert hex format to a rgb color
+function rgb2hex(rgb) {
+	rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+	return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+
+function hex(x) {
+	return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+}
